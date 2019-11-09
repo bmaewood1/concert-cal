96a084333f84bf4b5afa4efe942dd4667c970594
Bailey Wood <woodbmae@gmail.com>
2019-11-09 13:49:31 -0500

    api

--- app/api.rb
+++ app/api.rb
@@ -1,17 +1,24 @@
 require 'bundler'
 Bundler.require
-require_all "app"
-require 'byebug'
-require "action_cable/engine"
 
+
+require 'byebug'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.sqlite3')
 
 json = JSON.parse(RestClient.get 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&marketId=35&apikey=9Gi3bYz1RDWYyebA5yy6setBUgOQPG12')
 
 
+event_hash = {
+    artist: nil,
+    venue: nil,
+    date: nil
+}
+
 json["_embedded"]["events"].each do |main|
-    Event.create(artist: main["name"], venue: main["_embedded"]["venues"].first["name"], date: main["dates"]["start"]["localDate"])
+    event_hash[:artist] = main["name"]
+    event_hash[:venue] = main["_embedded"]["venues"].first["name"]
+    event_hash[:date] = main["dates"]["start"]["localDate"])
     byebug
 end
 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,61 @@
+require 'bundler'
+Bundler.require
+require 'byebug'
+
 class EventsController < ApplicationController
+
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.sqlite3')
+
+  # json = JSON.parse(RestClient.get 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&marketId=35&apikey=9Gi3bYz1RDWYyebA5yy6setBUgOQPG12')
+
+  # event_array = []
+
+  # event_hash = {
+  #   artist: nil,
+  #   venue: nil,
+  #   date: nil
+  # }
+
+  # json["_embedded"]["events"].each do |main|
+  #     event_hash[:artist] = main["name"]
+  #     event_hash[:venue] = main["_embedded"]["venues"].first["name"]
+  #     event_hash[:date] = main["dates"]["start"]["localDate"]
+  #     event_hash << event_array
+  #     byebug
+  # end
+
   def index
-    @events = Event.all
-  end
+    json = JSON.parse(RestClient.get 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&marketId=35&apikey=9Gi3bYz1RDWYyebA5yy6setBUgOQPG12')
+
+    @event_array = []
+
+    event_hash = {
+      artist: nil,
+      venue: nil,
+      date: nil
+    }
+  
+    json["_embedded"]["events"].select do |main|
+        event_hash[:artist] = main["name"]
+        event_hash[:venue] = main["_embedded"]["venues"].first["name"]
+        event_hash[:date] = main["dates"]["start"]["localDate"]
+        counter = 0
+        while counter < 5 
+          @event_array[counter] = event_hash
+        end
+        byebug
+      end
+    
+    
+      # count = 0
+      # @event_array[count] = event_hash
+      # count += 1
+      # byebug
+
+    end
 
   def show
     @event = Event.find(params[:id])
   end
-  
+
 end

c23825714bf391d8bcd6c8aef70251edda9dabe8
Bailey Wood <woodbmae@gmail.com>
2019-11-09 14:34:24 -0500

    events index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,24 +27,34 @@ class EventsController < ApplicationController
   def index
     json = JSON.parse(RestClient.get 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&marketId=35&apikey=9Gi3bYz1RDWYyebA5yy6setBUgOQPG12')
 
-    @event_array = []
+    @events = []
 
-    event_hash = {
-      artist: nil,
-      venue: nil,
-      date: nil
-    }
+    # event_hash = {
+    #   artist: nil,
+    #   venue: nil,
+    #   date: nil
+    # }
   
-    json["_embedded"]["events"].select do |main|
-        event_hash[:artist] = main["name"]
-        event_hash[:venue] = main["_embedded"]["venues"].first["name"]
-        event_hash[:date] = main["dates"]["start"]["localDate"]
-        counter = 0
-        while counter < 5 
-          @event_array[counter] = event_hash
+      # counter = 0
+      # while counter < 10
+      #   json["_embedded"]["events"].each do |main|
+      #     event_hash[:artist] = main["name"]
+      #     event_hash[:venue] = main["_embedded"]["venues"].first["name"]
+      #     event_hash[:date] = main["dates"]["start"]["localDate"]
+      #     @event_array[counter] = event_hash
+      #     counter += 1
+      #     byebug
+      #   end
+      # end
+
+      counter = 0
+      while counter < 5
+        json["_embedded"]["events"].each do |main|
+          @events[counter] = Event.create(artist: main["name"], venue: main["_embedded"]["venues"].first["name"], date: main["dates"]["start"]["localDate"])
+          counter += 1
         end
-        byebug
       end
+
     
     
       # count = 0
